03548702d295f86eaec7d1d7a260880b5f1f8e9f
kushalg36 <kushalg36@gmail.com>
2019-08-14 09:29:01 +0530

    basic complete

--- bit/src/components/OpenIssues.js
+++ bit/src/components/OpenIssues.js
@@ -11,18 +11,34 @@ class openIssues extends Component {
     componentDidMount(){
         axios.post('http://localhost:4000/api/issues')
         .then(issues => {
-            console.log(issues);
-            // this.setState({
-            //     issues: issues.data
-            // })
+            this.setState({
+                issues: issues.data
+            })
         })
     }
 
     render() {
+
+        const { issues } = this.state;
+        const issueList = issues.length ? (
+            issues.map(issue => {
+                return(
+                    <div className="post card" key={issue._id}>
+                        <div className="card-content">
+                            <span className="card-title">{issue.subject}</span>
+                            <p>{issue.logic}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center"><h2>No Issues yet!</h2></div>
+        )
+
         return (
             <div>
                 <div className="container">
-                    <h1 className="center">Hello</h1>
+                    {issueList}
                 </div>
             </div>
         )
